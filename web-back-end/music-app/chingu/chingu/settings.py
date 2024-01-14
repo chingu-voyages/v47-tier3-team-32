951e33eec0b73647aebae58d963d061959e14d8b
claytoncritchfield <clayton.critchfield@gmail.com>
2024-01-14 15:26:36 -0500

    Add configuration for postgreSQL settings to settings.py

--- web-back-end/music-app/chingu/chingu/settings.py
+++ web-back-end/music-app/chingu/chingu/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'chingu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chinguv47', 
+        'USER': 'postgres',
+        'PASSWORD': 'pa$$word',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
